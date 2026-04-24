53acf2dc27f85313b8e1cfd576443fb6ab11b8dd
Ali Yasin G <aygmedia7@gmail.com>
2026-04-24 13:12:36 +0300

    Implemented batch inversion over finite fields

--- src/vanity_addr_generator/mod.rs
+++ src/vanity_addr_generator/mod.rs
@@ -1,6 +1,6 @@
 //! # Vanity Address Generation Module
 //!
-//! This module is the core of btc-vanity. It provides the functionality to generate Bitcoin vanity addresses.
+//! This module is the core of btc-vanity. It provides the functionality to generate Bitcoin vanity addresses./
 
 pub mod chain;
 pub mod vanity_addr;
