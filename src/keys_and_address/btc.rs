e179dc2f644fb05b13cdc1138aa1c0189e8dac9a
Ali Yasin G <aygmedia7@gmail.com>
2026-05-15 22:14:12 +0300

    Update btc.rs

--- src/keys_and_address/btc.rs
+++ src/keys_and_address/btc.rs
@@ -1,5 +1,5 @@
 //! # Bitcoin Key Pair and Address Generation
-//12
+//1
 //! This module provides functionality to generate Bitcoin key pairs and their associated compressed addresses.
 
 use std::cell::RefCell;
